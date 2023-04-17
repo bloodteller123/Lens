2a4f5657165da1429ecf894b272a80726f38d480
bloodteller123 <chenzihan708@gmail.com>
2023-04-17 15:38:05 -0400

    test env

--- src/lens/Apollo.js
+++ src/lens/Apollo.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-const APIURL = 'https://api.lens.dev/';
+const APIURL = process.env.NODE_ENV ==='production'? 'https://api.lens.dev/' : 'https://api-mumbai.lens.dev/';
 //https://www.apollographql.com/docs/react/get-started
 const Client = new ApolloClient({
     uri: APIURL,
